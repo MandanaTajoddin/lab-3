efd17ba5da286fffb6075aef94b81015329b6926
MandanaTajoddin <40220065+MandanaTajoddin@users.noreply.github.com>
2018-06-27 20:19:56 -0400

    changed

---
+++ main.js
@@ -0,0 +1,15 @@
+let name = prompt("What\'s your name?");
+console.log(name);
+let origin = prompt("Where are you from " + name);
+let resident = prompt("Are you a lawful resident of US " + name);
+let date = prompt("When did you enter US from " + origin);
+let job = prompt("What is your educational degree " + name);
+console.log(origin);
+console.log(resident);
+console.log(date);
+console.log(job);
+console.log(alert("Let\'s go " + name));
+let myArray = ["name","origin","job"]
+for (let i=0; i<myArray.leanght; i++){
+    console.log(myArray + "approved")
+}
